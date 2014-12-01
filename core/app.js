ab6c523176b411d614bea27e1665ee54cda4a30b
alexcdahl <alex.c.dahl@gmail.com>
2014-12-01 14:30:27 -0700

    Added SASS integration

--- core/app.js
+++ core/app.js
@@ -4,11 +4,37 @@ var serve = require('koa-static')
 var render = require('koa-ejs')
 var path = require('path')
 
+var sass = require('node-sass');
+var fs = require('fs');
+
 var config=require('./config1')
 
 var app = koa();
 
 var auth = require('./app_modules/auth')
+
+if(config.debug) {
+  app.use(function *(next) {
+    var matches = this.request.url.match(/^\/css\/(.*)\.css$/);
+    if(matches) {
+      var filename = matches[1];
+      var pubdir = path.join(__dirname, '..', 'public', 'css');
+      var sassdir = path.join(__dirname, 'sass');
+
+      try {
+        var css = sass.renderSync({
+          file: path.join(sassdir, filename + '.scss'),
+        });
+      }catch(e) {
+        console.error(e);
+        yield next;
+        return;
+      }
+      fs.writeFileSync(path.join(pubdir, filename + '.css'), css);
+    }
+	yield next;
+  });
+}
 
 app.use(serve(path.join(__dirname, '..', 'public')));
 
@@ -20,7 +46,7 @@ render(app, {
   cache: !config.debug,
   debug: config.debug,
   locals: {
-    title: 'HLRDesk'
+  title: 'HLRDesk'
   }
 });
 
