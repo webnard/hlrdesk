c3e48d6103aa557f372b9f50037743fad5453aea
trevor <trevren11@gmail.com>
2014-12-11 12:09:52 -0700

    Added left panel to homepage

---
+++ public/js/admin.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+var div = $(this);
+$(".lpanel").hover(function(){
+  $(this).css("background-color","#8c8686");
+  },function(){
+  $(this).css("background-color","#2b3044");
+});
+
+});
