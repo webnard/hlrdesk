a639b080448553a5dffd0f4c0e18c8b7ada65cf3
trevor <trevren11@gmail.com>
2014-12-11 15:09:40 -0700

    Changes to panel

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
 
-var div = $(this);
-$(".lpanel").hover(function(){
-  $(this).css("background-color","#8c8686");
-  },function(){
-  $(this).css("background-color","#2b3044");
-});
-
 });
