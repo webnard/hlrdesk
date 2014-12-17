2348e5f4a1715edc784424093d9763344e29c67f
Ian Hunter <beanland2@gmail.com>
2014-12-17 11:14:15 -0700

    Removing config file in favor of environment variables.

--- core/app_modules/auth.js
+++ core/app_modules/auth.js
@@ -1,5 +1,4 @@
 //Gets developers personal config Information
-var config=require('../config1')
 var cas = require('byu-cas');
 
 module.exports = {
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,38 +2,11 @@
  * See http://jasmine.github.io/2.0/introduction.html for documentation.
  */
 
-// make it so loading config1.js actually loads the template, config.js
-// this helps make sure these tests don't Bjork anything on the live server
-// (if they are run). We can also easily monkeypatch the template to be
-// test-specific
-var mockery = require('mockery');
-
-mockery.enable();
-mockery.warnOnUnregistered(false);
-
-// TODO: come up with a less terrible way of mocking the configuration. Environment vars?
-mockery.registerMock('../../core/config1', require('../core/config'));
-mockery.registerMock('../core/config1', require('../core/config'));
-mockery.registerMock('../config1', require('../core/config'));
-mockery.registerMock('config1', require('../core/config'));
-
-var config = require('../core/config1');
-
-/**
- * Patch config for testing purposes as follows, and then modules that require
- * the configuration file will get the updated values.
- *
- * Example: config.localhost = 'thing';
- */
-config.localhost = 'http://localhost';
-config.port = 8080;
-
 var expect     = require('expect.js'),
     auth       = require('../core/app_modules/auth');
 
 var MOCK_USERNAME = 'prabbit',
-    MOCK_PASSWORD = 'prabbitprabbit1',
-    SERVICE = config.localhost + ':' + config.port + '/signin';
+    MOCK_PASSWORD = 'prabbitprabbit1';
 
 describe('auth', function() {
 
