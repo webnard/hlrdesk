6b51e05002afd9c4eabe225140e303fa65610f4c
Ian Hunter <beanland2@gmail.com>
2014-12-16 23:08:22 -0700

    Staging process now returns to previous branch after pushing up changes.

--- .stage.sh
+++ .stage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
+test $(git diff --cached --numstat | wc -l) -gt 0 && echo "Files are staged; I refuse to push to staging unless you stash or commit these changes." && exit 1
 git branch -D dokku-staging
 git checkout -b dokku-staging
 npm run compile-sass
 git add -f public/css/*
 git commit -m 'Staging'
 git push dokku@hlrdesk-staging-dokku:hlrdesk-staging dokku-staging:master -f
-git checkout $CURRENT_BRANCH
-
-echo "NOTE: Currently on branch dokku-staging"
+git checkout -
+git branch -D dokku-staging
