5346d760c81615728f3fa23eb247448a2d324f64
trevor <trevren11@gmail.com>
2014-12-18 13:59:58 -0700

    adding google analytics

---
+++ core/config1.js
@@ -0,0 +1,5 @@
+exports.localhost="http://hlr.byu.edu";  //Your Local host here
+exports.port =8080; //Your port number here
+exports.url = exports.localhost+":"+exports.port.toString();
+exports.debug=true;
+exports.database = "postgres://postgres:password@localhost/hlrdesk"//Your db location formatted like so
