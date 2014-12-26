85c6401f3d029d7788ed3105ac18c29965225410
Ian Hunter <beanland2@gmail.com>
2014-12-26 14:35:06 -0700

    Deployment to development server.

---
+++ .deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+case "$1" in
+  dev)
+    BRANCH=dokku-devel
+    REMOTE=hlrdev.byu.edu
+    APP=ianh
+    DB=hlrdesk
+
+    ssh dokku@$REMOTE postgresql:delete $DB
+    ssh dokku@$REMOTE postgresql:create $DB
+    ssh dokku@$REMOTE postgresql:restore $DB < core/db/schema.sql
+    ssh dokku@$REMOTE postgresql:link $APP $DB
+    ;;
+  staging)
+    BRANCH=dokku-staging
+    REMOTE=hlrdesk-staging.byu.edu
+    APP=hlrdesk-staging
+    DB=hlrdesk
+    ;;
+  production)
+    echo "Production server not set up yet."
+    exit 2
+    ;;
+  *)
+    echo "Usage: npm run deploy {dev|staging|production}"
+    exit 3
+esac
+
+test $(git diff --cached --numstat | wc -l) -gt 0 && echo -e "I'm sorry, $USER; I'm afraid I can't do that.\nFiles are currently staged. I refuse to push to the server unless you stash or commit these changes." && exit 1
+
+git branch -D $BRANCH
+git checkout -b $BRANCH
+npm run compile-sass
+git add -f public/css/*
+git commit -m "$1"
+git push dokku@$REMOTE:$APP $BRANCH:master -f
+git checkout -
+git branch -D $BRANCH
