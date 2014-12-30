3ea9c0a61ad6017d1d1a3044a596a15b0b65ce69
Ian Hunter <beanland2@gmail.com>
2014-12-30 13:45:15 -0700

    Authentication module provides a way to retrieve CAS attributes outside of the BYU domain

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -10,6 +10,20 @@ var MOCK_USERNAME = 'prabbit',
 
 describe('auth', function() {
 
+  describe('#has_cas_access()', function() {
+    it('should be false on a non-BYU domain', function() {
+      expect(auth.has_cas_access('localhost', 443)).to.be(false);
+    });
+
+    it('should be false on a non-443 port', function() {
+      expect(auth.has_cas_access('hlrdesk.byu.edu', 80)).to.be(false);
+    });
+
+    it('should be true on a byu domain on port 443', function() {
+      expect(auth.has_cas_access('hlrdesk.byu.edu', 443)).to.be(true);
+    });
+  });
+
   describe('#cas_login()', function() {
     // do something
   });
