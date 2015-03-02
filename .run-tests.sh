93fa150c95ae5ae5f4edecc9605ca505704fb86a
Ian Hunter <beanland2@gmail.com>
2015-03-02 10:22:09 -0700

    Fixes issues with first-time testing.

--- .run-tests.sh
+++ .run-tests.sh
@@ -9,16 +9,16 @@ MOCK_DATA_FILE=core/db/mock-data.sql
 export $(./.env.sh)
 
 printf "Creating and populating test database $TEST_DB"
-dropdb $TEMPLATE_DB >/dev/null 2>&1
+dropdb $TEMPLATE_DB --if-exists > /dev/null
 printf "."
-createdb $TEMPLATE_DB >/dev/null
+createdb $TEMPLATE_DB > /dev/null
 printf "."
-psql $TEMPLATE_DB < $SCHEMA_FILE >/dev/null
+psql $TEMPLATE_DB < $SCHEMA_FILE > /dev/null
 printf "."
-psql $TEMPLATE_DB < $MOCK_DATA_FILE >/dev/null
+psql $TEMPLATE_DB < $MOCK_DATA_FILE > /dev/null
 printf "."
-dropdb $TEST_DB >/dev/null 2>&1
+dropdb $TEST_DB --if-exists > /dev/null
 printf "."
-createdb $TEST_DB -T $TEMPLATE_DB >/dev/null
+createdb $TEST_DB -T $TEMPLATE_DB > /dev/null
 printf " Done!\n"
 NODE_TEST=true PGPOOLSIZE=0 PGDATABASE=$TEST_DB TEMPLATE_DB=$TEMPLATE_DB node --harmony $(npm bin)/istanbul cover _mocha -- --require co-mocha --harmony tests/ "$@"
