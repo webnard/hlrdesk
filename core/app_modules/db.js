a155d6def0853112a1387a32d41e6908b0676c36
Ian Hunter <beanland2@gmail.com>
2015-03-03 13:36:19 -0700

    Uses database connection string if present. Fixes issue with PG's changing ports on server reboots.

--- core/app_modules/db.js
+++ core/app_modules/db.js
@@ -1,5 +1,6 @@
 //var pg = require('pg');
 var pg = require('postgres-gen');
+
 var defaults = {};
 defaults.user = process.env.PGUSER;
 defaults.password = process.env.PGPASS;
@@ -9,6 +10,6 @@ defaults.poolSize = process.env.PGPOOLSIZE || 10;
 defaults.pool = (process.env.PGPOOLSIZE === "0" ? false : 10);
 //module.exports = pg;
 module.exports = function(){
-  return pg(defaults);
+  return pg(process.env.DATABASE_URL || defaults);
 };
 
