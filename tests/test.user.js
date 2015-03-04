40fd2097fb2db6c2a61c1898e02d4f9df7b06c94
Ian Hunter <beanland2@gmail.com>
2015-03-04 15:32:57 -0700

    Updating tests to work with Chai.

--- tests/test.user.js
+++ tests/test.user.js
@@ -1,27 +1,20 @@
-var expect  = require('expect.js'),
+var chai    = require('chai'),
+    expect  = chai.expect,
     user    = require('../core/app_modules/user');
 
 describe('user', function() {
   describe('#update(username, properties)', function() {
-    it('should not allow me to update the username', function* (done) {
-      try {
-        user.update('njuster', {netid: 'somethingnew'});
-      }catch(e) {
-        done();
-      }
+    it('should not allow me to update the username', function* () {
+      return expect(user.update('njuster', {netid: 'somethingnew'})).to.eventually.be.rejected;
     });
     it('should return true on success', function* () {
       var data =  {phone: '8015554567', email: 'ham@example.com'}
       var result = yield user.update('njuster', data);
-      expect(result).to.be(true);
+      expect(result).to.be.true;
     });
     it('should throw an error for an invalid email', function* (done) {
-      try {
-        var data = {email: 'notanemail'}
-        yield user.update('njuster', data);
-      }catch(e) {
-        done();
-      }
+      var data = {email: 'notanemail'}
+      return expect(user.update('njuster', data)).to.eventually.be.rejected;
     });
   });
 });
