97a85c255c29cd81b962eb6df019c7514a6ece72
Ian Hunter <beanland2@gmail.com>
2015-03-17 15:15:59 -0600

    Slightly cleans up Casper's check-in test.

--- tests/casperjs/casper.check-in.js
+++ tests/casperjs/casper.check-in.js
@@ -7,20 +7,18 @@ casper.options.viewportSize = {top: 0, left: 0, width: 1280, height: 720};
 casper.test.begin('check in', function(test) {
   casper.start(BASE + '/logmein?as=prabbit', function() {
     test.assertHttpStatus(200, "Login as prabbit");
-  });
-  casper.thenOpen(BASE, function() {
+  })
+ .thenOpen(BASE, function() {
     test.assertExists('.lpanel.check-in');
     this.click('.lpanel.check-in');
-  });
-  casper.then(function() {
-    this.waitFor(function(){return casper.exists('#checked-out-items')}, function then() {
+  })
+  .then(function() {
+    casper.waitFor(function(){return casper.exists('#checked-out-items')}, function then() {
       test.assertExists('#checked-out-items');
       casper.capture(SHOTS + 'checked-out-items-list.png');
     });
-  });
-  casper.then(function() {
-    test.done();
-  });
+  })
+  .then(function(){test.done()})
+  .run();
 });
 
-casper.run();
