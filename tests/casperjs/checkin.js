232f734d630d027f76b73c11d65602a65f6abdc4
Ian Hunter <beanland2@gmail.com>
2015-03-17 21:43:44 -0600

    Test showing how the back button works properly in Firefox

--- tests/casperjs/checkin.js
+++ tests/casperjs/checkin.js
@@ -2,25 +2,22 @@ const PORT = require('system').env.PORT;
 const BASE = 'http://127.0.0.1:' + PORT;
 const SHOTS = 'tests/screenshots/';
 
-casper.options.viewportSize = {top: 0, left: 0, width: 1280, height: 720};
-
 casper.test.begin('check in', function(test) {
+  casper.options.viewportSize = {top: 0, left: 0, width: 1280, height: 720};
+
   casper.start(BASE + '/logmein?as=prabbit', function() {
     test.assertHttpStatus(200, "Login as prabbit");
-  });
-  casper.thenOpen(BASE, function() {
+  })
+ .thenOpen(BASE, function() {
     test.assertExists('.lpanel.check-in');
     this.click('.lpanel.check-in');
-  });
-  casper.then(function() {
-    this.waitFor(function(){return casper.exists('#checked-out-items')}, function then() {
+  })
+  .then(function() {
+    casper.waitFor(function(){return this.exists('#checked-out-items')}, function then() {
       test.assertExists('#checked-out-items');
       casper.capture(SHOTS + 'checked-out-items-list.png');
     });
-  });
-  casper.then(function() {
-    test.done();
-  });
+  })
+  .then(function(){casper.clear(); test.done()})
+  .run();
 });
-
-casper.run();
