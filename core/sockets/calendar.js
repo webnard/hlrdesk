b897e669fc70a9c259c20f260873063ed6175c40
Ian Hunter <beanland2@gmail.com>
2015-03-19 21:24:02 -0600

    Sockets moved to separate files.

---
+++ core/sockets/calendar.js
@@ -0,0 +1,27 @@
+var db = require('../app_modules/db');
+var redis = require("../app_modules/redis");
+
+module.exports = function messages(socket, app) {
+  socket.on('calendar event', function(event) {
+    var cal = require('../app_modules/cal');
+    var redisClient = redis();
+    redisClient.smembers(cookie.parse(event._cookie).token, function(err, reply){
+      var reply = reply;
+      var username = reply.toString('utf8');
+      cal.addCalendarEvent(username, event, reply).then(function() {
+        app.io.emit("calendar event", event);
+      });
+    });
+  });
+
+  socket.on('delete calendar event', function(event) {
+    var cal = require('../app_modules/cal');
+    var redisClient = redis();
+    redisClient.smembers(cookie.parse(event._cookie).token, function(err, reply){
+      var username = reply.toString('utf8');
+      cal.deleteCalendarEvent(username, event).then(function() {
+        app.io.emit("delete calendar event", event);
+      });
+    });
+  });
+};
