6553fe4898d92e35b5dd95efcd4eabe6cb0b0db4
Ian Hunter <beanland2@gmail.com>
2015-03-20 21:35:17 -0600

    Fixing calendar issue I caused in the last commit.

--- core/sockets/calendar.js
+++ core/sockets/calendar.js
@@ -1,12 +1,12 @@
 var db = require('../app_modules/db');
 var redis = require("../app_modules/redis");
+var cookie = require('cookie');
 
 module.exports = function messages(socket, app) {
   socket.on('calendar event', function(event) {
     var cal = require('../app_modules/cal');
     var redisClient = redis();
     redisClient.smembers(cookie.parse(event._cookie).token, function(err, reply){
-      var reply = reply;
       var username = reply.toString('utf8');
       cal.addCalendarEvent(username, event, reply).then(function() {
         app.io.emit("calendar event", event);
