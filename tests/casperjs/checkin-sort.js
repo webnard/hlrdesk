07e8098504bd786849ed45e0a1237a13ce832bbe
Ian Hunter <beanland2@gmail.com>
2015-03-27 16:54:43 -0600

    Further attempts to fix CasperJS tests.

--- tests/casperjs/checkin-sort.js
+++ tests/casperjs/checkin-sort.js
@@ -7,9 +7,6 @@ const SHOTS = 'tests/screenshots/';
 casper.test.begin('change sorting of checkin with column clicks', function(test) {
   casper.options.viewportSize = {top: 0, left: 0, width: 1280, height: 720};
 
-  var order_asc_top = null;
-  var order_desc_bottom = null;
-
   casper.start(BASE + '/logmein?as=prabbit', function() {
     test.assertHttpStatus(200, "Login as prabbit");
   });
@@ -19,17 +16,18 @@ casper.test.begin('change sorting of checkin with column clicks', function(test)
   });
   casper.waitForSelector('#checked-out-items .col-due.header');
   casper.thenClick('#checked-out-items .col-due');
+  casper.wait(250);
   casper.then(function() {
-    order_asc_top = this.evaluate(function(){return document.querySelector('#checked-out-items tr:first-child td[data-date]').getAttribute('data-date');});
     casper.capture(SHOTS + 'checkin-sort-date-asc.png');
   });
-  casper.thenClick('#checked-out-items .col-due');
-  casper.wait(500);
   casper.then(function() {
-    order_desc_bottom = this.evaluate(function(){return document.querySelector('#checked-out-items tr:last-child td[data-date]').getAttribute('data-date');});
-    casper.capture(SHOTS + 'checkin-sort-date-desc.png');
-
-    test.assertEquals(order_asc_top, order_desc_bottom);
+    var order_asc_top = this.evaluate(function(){return document.querySelector('#checked-out-items tr:last-child td[data-date]').getAttribute('data-date');});
+    this.click('#checked-out-items .col-due');
+    this.wait(250).then(function() {
+      casper.capture(SHOTS + 'checkin-sort-date-desc.png');
+      var order_desc_bottom = this.evaluate(function(){return document.querySelector('#checked-out-items tr:first-child td[data-date]').getAttribute('data-date');});
+      test.assertEquals(order_asc_top, order_desc_bottom);
+    });
   });
   casper.then(function(){casper.clear(); test.done()})
   casper.run();
