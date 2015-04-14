7823483da4786f21a7fca094b20f68222454d84f
Ian Hunter <beanland2@gmail.com>
2015-04-13 22:01:41 -0600

    Adding in socket tests for inventory checkout.

--- tests/test.sockets.inventory.js
+++ tests/test.sockets.inventory.js
@@ -25,5 +25,48 @@ describe('socket: inv.search', function() {
     });
     socket.emit('inv.search', {'text': 'HELLO', token: socket.__token});
   });
+});
 
+describe('socket: inv.checkout', function() {
+  it('should emit inv.checkout.success on success', function*(done) {
+    var server = app.listen(process.env.PORT);
+    var socket = yield client(server, 'prabbit');
+
+    socket.on('inv.checkout.success', done);
+
+    var due = new Date();
+    due = due.setYear(due.getUTCFullYear()+1);
+    var data = {
+      items: [{
+        'call': 'M347FEST',
+        'copy': 1,
+        'due': due
+      }],
+      netid: 'milo',
+      email: 'test@example.com',
+      phone: '0118 999 881 99 9119 7253'
+    };
+    socket.emit('inv.checkout', data);
+  });
+  it('should emit error on error', function*(done) {
+    var server = app.listen(process.env.PORT);
+    var socket = yield client(server, 'prabbit');
+
+    socket.on('error', done);
+
+    var due = new Date();
+    // due date is earlier than it should be
+    due = due.setYear(due.getUTCFullYear()-1);
+    var data = {
+      items: [{
+        'call': 'M347FEST',
+        'copy': 1,
+        'due': due
+      }],
+      netid: 'milo',
+      email: 'test@example.com',
+      phone: '0118 999 881 99 9119 7253'
+    };
+    socket.emit('inv.checkout', data);
+  });
 });
