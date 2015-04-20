08c3787724a9d526f1b92e34b9b720062f351bea
Ian Hunter <beanland2@gmail.com>
2015-04-20 15:25:44 -0600

    Semicolons in auth.js for codeclimate.

--- core/app_modules/auth.js
+++ core/app_modules/auth.js
@@ -7,22 +7,22 @@ var redis = require('./redis');
 
 check_id = co.wrap(function*(netid){
   var client = db();
-  var is_user = yield client.query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE netid = $1) THEN 'TRUE' ELSE 'FALSE' end;", [netid])
+  var is_user = yield client.query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE netid = $1) THEN 'TRUE' ELSE 'FALSE' end;", [netid]);
   return yield Promise.resolve(is_user.rows[0].case == "TRUE");
-})
+});
 
 check_admin = co.wrap(function*(user){
   var client = db();
-  var is_user = yield client.query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE netid = $1 AND admin = 't') THEN 'TRUE' ELSE 'FALSE' end;", [user])
+  var is_user = yield client.query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE netid = $1 AND admin = 't') THEN 'TRUE' ELSE 'FALSE' end;", [user]);
   console.warn("The function 'check_admin' is deprecated. Use 'isAdmin' instead.");
   return yield Promise.resolve(is_user.rows[0].case == "TRUE");
-})
+});
 
 isAdmin = co.wrap(function*(user){
   var client = db();
-  var is_user = yield client.query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE netid = $1 AND admin = 't') THEN 'TRUE' ELSE 'FALSE' end;", [user])
+  var is_user = yield client.query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE netid = $1 AND admin = 't') THEN 'TRUE' ELSE 'FALSE' end;", [user]);
   return yield Promise.resolve(is_user.rows[0].case == "TRUE");
-})
+});
 
 module.exports = {
   cas_login: function(ticket, service) {
@@ -56,7 +56,7 @@ module.exports = {
 
     redisClient.sadd([token, obj.username]);
     redisClient.expire(token, 43200);
-    client.query("INSERT INTO users(netid) VALUES ($1);", [obj.username] )
+    client.query("INSERT INTO users(netid) VALUES ($1);", [obj.username] );
   },
 
   getUser: co.wrap(function* (token) {
@@ -99,7 +99,7 @@ module.exports = {
     var is_user = yield check_id(netid);
     var user_is_admin = yield check_admin(user);
     if (is_user && user_is_admin){
-      client.query("UPDATE users SET admin='TRUE' WHERE netid = $1", [netid])
+      client.query("UPDATE users SET admin='TRUE' WHERE netid = $1", [netid]);
       return yield Promise.resolve(true);
     }
     else {
