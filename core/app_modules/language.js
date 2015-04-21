1ce01133bfa55ecb618fed676809107ba1df9b92
Ian Hunter <beanland2@gmail.com>
2015-04-20 19:30:43 -0600

    Language API functional.

--- core/app_modules/language.js
+++ core/app_modules/language.js
@@ -2,15 +2,22 @@ var co = require('co');
 var db = require('./db');
 
 module.exports = {
-  create: co.wrap(function*(code, language) {
-
+  create: co.wrap(function*(code, name) {
+    yield db().nonQuery('INSERT INTO LANGUAGES(code, name) VALUES($1, $2)', [code, name]);
+    return yield Promise.resolve(true);
   }),
-  update: co.wrap(function*(code, new_code, new_language) {
-
+  update: co.wrap(function*(code, new_code, new_name) {
+    yield db().nonQuery('UPDATE LANGUAGES SET code=$2, name=$3 WHERE code=$1', [code, new_code, new_name]);
+    return yield Promise.resolve(true);
   }),
-  // delete is a reserved word
+  // delete is a reserved word, so I'm using 'remove'
   remove: co.wrap(function*(code) {
-
+    var client = db();
+    var rowsAffected = yield client.nonQuery('DELETE FROM languages WHERE code=$1', [code]);
+    if(rowsAffected < 1) {
+      return yield Promise.reject('Could not delete language code ' + code);
+    }
+    return yield Promise.resolve(true);
   })
 }
 
