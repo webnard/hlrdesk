d7eb7613d0ebbfc006d83b46735cc3265c041406
Ian Hunter <beanland2@gmail.com>
2015-04-20 19:18:24 -0600

    Adding in tests for language.

--- tests/test.language.js
+++ tests/test.language.js
@@ -14,8 +14,50 @@ describe('language', function() {
     });
     it('should contain a code and a name', function*() {
       var languages = yield language.list;
-      var first = [0];
+      var first = languages[0];
       expect(first).to.contain.keys(['code','name']);
+    });
+  });
+  describe('#remove(code)', function() {
+    it('should return true on success', function*() {
+      var response = yield language.remove('eng');
+      expect(response).to.be.true;
+    });
+    it('should be rejected if the code does not exist', function*() {
+      var promise = language.remove('yyy');
+      expect(promise).to.eventually.be.rejected;
+    });
+    it('should reduce the length of language.list', function*() {
+      var l1 = yield language.list;
+      yield language.remove('eng');
+      var l2 = yield language.list;
+      expect(l1).to.have.length.above(l2.length);
+    });
+  });
+  describe('#create(code, language)', function() {
+    it('should be rejected if the code already exists', function*() {
+      var promise = language.create('eng', 'Denny\'s Moons Over My Hammy®');
+      expect(promise).to.eventually.be.rejected;
+    });
+    it('should increase the length of list', function*() {
+      var l1 = yield language.list;
+      yield language.create('yyy', 'Boghog');
+      var l2 = yield language.list;
+      expect(l1).to.have.length.below(l2.length);
+    });
+  });
+  describe('#update(code, new_code, new_language', function() {
+    it('should be rejected if the current code does not exist', function*() {
+      var promise = language.update('yyy', 'yyy', 'Zug-Zug');
+      expect(promise).to.eventually.be.rejected;
+    });
+    it('should be rejected if the new code already exists', function*() {
+      var promise = language.update('eng', 'fra', 'Franglish');
+      expect(promise).to.eventually.be.rejected;
+    });
+    it('should resolve true on success', function*() {
+      var result = yield language.update('eng', 'eng', 'Cool English.');
+      expect(result).to.be.true;
     });
   });
 });
