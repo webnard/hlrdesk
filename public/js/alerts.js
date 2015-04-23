b4555e5020b718676afb0951699486ca50e30f7d
Ian Hunter <beanland2@gmail.com>
2015-04-23 13:57:22 -0600

    Ability to edit languages.
    Also:
     * Removes a significant number of languages not currently used in our prod server.
     * New session for plain ol' student.
     * Flash message support

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -7,12 +7,32 @@ window.HLRDESK = window.HLRDESK || {};
     throw "Pattern library's displayModal method not found!";
   }
 
+  var FLASH_MESSAGE_DURATION = 1000;
+
+  // should match fadeout time in flash-message.scss
+  var FLASH_MESSAGE_FADEOUT_TIME = 500;
+
   H.alert = {
     error: function(message) { // see core/templates/error-modal.html
       var tpl = document.getElementById('tpl-error-modal');
       var el = document.importNode(tpl.content, true);
       el.querySelector('.error-message').textContent = message;
       pl.displayModal(el);
+    },
+
+    flash: function(message) {
+      var el = document.createElement('div');
+      el.classList.add('flash-message');
+      el.textContent = message;
+      var pane = document.getElementById('loader');
+      pane.insertBefore(el, pane.childNodes[0]);
+
+      setTimeout(function() {
+        el.classList.add('fadeout');
+        setTimeout(function() {
+          pane.removeChild(el);
+        }, FLASH_MESSAGE_FADEOUT_TIME);
+      }, FLASH_MESSAGE_DURATION);
     }
   }
 
