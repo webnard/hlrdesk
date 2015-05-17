65ff4831f4518adf46a3c4d92b6220ee630b6104
Ian Hunter <beanland2@gmail.com>
2015-05-16 20:18:16 -0600

    Conversion script from checkout to new checked out table.

--- conversion/conversion.sql
+++ conversion/conversion.sql
@@ -1,1 +1,7 @@
-SELECT COPY copy, NETID netid, DUEDATETIME due FROM `checkout` WHERE 1;
+DROP VIEW IF EXISTS new_checked_out;
+
+CREATE VIEW new_checked_out AS
+  SELECT `COPY` `copy`, `callnumber` `call`, `NETID` `netid`, `DUEDATETIME` `due`,
+  '::ROBO::' `attendant`
+  FROM `checkout` JOIN
+  titles ON titles.AQNO = checkout.AQNO;
