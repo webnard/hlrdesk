d5c5f40020b3fd6f3aa7449d3b2a2acc4bb1fba3
Ian Hunter <beanland2@gmail.com>
2015-05-21 13:41:27 -0600

    Fixing a few missing things in the conversion script.

--- conversion/conversion.sql
+++ conversion/conversion.sql
@@ -1,12 +1,16 @@
+DROP VIEW IF EXISTS users_no_from_subquery_in_views;
+
+CREATE VIEW users_no_from_subquery_in_views AS
+  SELECT NetID netid, TRUE admin FROM employee
+  UNION ALL SELECT NetID netid, netid IN(SELECT NetID FROM employee) admin FROM reserveout
+  UNION ALL
+  SELECT NetID netid, netid IN(SELECT NetID FROM employee) admin FROM checkout
+  ;
+
 DROP VIEW IF EXISTS users;
 
 CREATE VIEW users AS
-  SELECT * FROM (
-    SELECT NetID netid, TRUE admin FROM employee
-    UNION ALL SELECT NetID netid, netid IN(SELECT NetID FROM employee) admin FROM reserveout
-    UNION ALL
-    SELECT NetID netid, netid IN(SELECT NetID FROM employee) admin FROM checkout
-  ) all_users GROUP BY netid
+  SELECT * FROM users_no_from_subquery_in_views GROUP BY netid;
   ;
 
 DROP VIEW IF EXISTS checked_out;
@@ -24,7 +28,7 @@ CREATE VIEW checked_out AS
   UNION ALL
 
   SELECT
-  `COPY` `copy`
+  `COPY` `copy`,
   `callnumber` `call`,
   `NETID` `netid`,
   `DUEDATETIME` `due`,
@@ -76,7 +80,7 @@ CREATE VIEW inventory AS
 DROP VIEW IF EXISTS media_items;
 
 CREATE VIEW media_items AS
-  SELECT description `medium`, call_number `call`
+  SELECT description `medium`, callnumber `call`
   FROM titles JOIN media ON media.code = titles.media;
 
 DROP VIEW IF EXISTS languages_items;
@@ -84,72 +88,73 @@ DROP VIEW IF EXISTS languages_items;
 CREATE VIEW languages_items AS
   SELECT
     CASE code
-      WHEN 'AF' THEN 'afr',
-      WHEN 'AM' THEN 'hye',
-      WHEN 'AR' THEN 'ara',
-      WHEN 'BU' THEN 'bul',
-      WHEN 'CA' THEN 'yue',
-      WHEN 'CB' THEN 'ceb',
-      WHEN 'CH' THEN 'cmn',
-      WHEN 'CM' THEN 'khm',
-      WHEN 'CZ' THEN 'ces',
-      WHEN 'DN' THEN 'dan',
-      WHEN 'DU' THEN 'nld',
-      WHEN 'EG' THEN 'eng',
-      WHEN 'EN' THEN 'eng',
-      WHEN 'EP' THEN 'epo',
-      WHEN 'EQ' THEN NULL, -- Equipment
-      WHEN 'ES' THEN 'eng', -- ESL
-      WHEN 'FJ' THEN 'fij',
-      WHEN 'FL' THEN 'fil',
-      WHEN 'FN' THEN 'fin',
-      WHEN 'FR' THEN 'fra',
-      WHEN 'GG' THEN 'eng', -- TODO: figure out the reason for all the English codes
-      WHEN 'GK' THEN 'ell',
-      WHEN 'GR' THEN 'deu',
-      WHEN 'HA' THEN 'haw',
-      WHEN 'HB' THEN 'heb',
-      WHEN 'HG' THEN 'hun',
-      WHEN 'HM' THEN 'hmn',
-      WHEN 'IC' THEN 'isl',
-      WHEN 'IN' THEN 'ind',
-      WHEN 'IT' THEN 'ita',
-      WHEN 'JP' THEN 'jpn',
-      WHEN 'KO' THEN 'kor',
-      WHEN 'LA' THEN 'lat',
-      WHEN 'LI' THEN NULL, -- Linguistics
-      WHEN 'LO' THEN 'lao',
-      WHEN 'MG' THEN 'mlg',
-      WHEN 'MO' THEN 'mon',
-      WHEN 'MU' THEN NULL, -- MUSIC
-      WHEN 'NO' THEN 'nor',
-      WHEN 'NR' THEN 'nor',
-      WHEN 'NV' THEN 'nav',
-      WHEN 'PG' THEN 'por',
-      WHEN 'PL' THEN 'pol',
-      WHEN 'PN' THEN 'fas',
-      WHEN 'QC' THEN 'quc',
-      WHEN 'RN' THEN 'ron',
+      WHEN 'AF' THEN 'afr'
+      WHEN 'AM' THEN 'hye'
+      WHEN 'AR' THEN 'ara'
+      WHEN 'BU' THEN 'bul'
+      WHEN 'CA' THEN 'yue'
+      WHEN 'CB' THEN 'ceb'
+      WHEN 'CH' THEN 'cmn'
+      WHEN 'CM' THEN 'khm'
+      WHEN 'CZ' THEN 'ces'
+      WHEN 'DN' THEN 'dan'
+      WHEN 'DU' THEN 'nld'
+      WHEN 'EG' THEN 'eng'
+      WHEN 'EN' THEN 'eng'
+      WHEN 'EP' THEN 'epo'
+      WHEN 'EQ' THEN NULL -- Equipment
+      WHEN 'ES' THEN 'eng' -- ESL
+      WHEN 'FJ' THEN 'fij'
+      WHEN 'FL' THEN 'fil'
+      WHEN 'FN' THEN 'fin'
+      WHEN 'FR' THEN 'fra'
+      WHEN 'GG' THEN 'eng' -- TODO: figure out the reason for all the English codes
+      WHEN 'GK' THEN 'ell'
+      WHEN 'GR' THEN 'deu'
+      WHEN 'HA' THEN 'haw'
+      WHEN 'HB' THEN 'heb'
+      WHEN 'HG' THEN 'hun'
+      WHEN 'HM' THEN 'hmn'
+      WHEN 'IC' THEN 'isl'
+      WHEN 'IN' THEN 'ind'
+      WHEN 'IT' THEN 'ita'
+      WHEN 'JP' THEN 'jpn'
+      WHEN 'KO' THEN 'kor'
+      WHEN 'LA' THEN 'lat'
+      WHEN 'LI' THEN NULL -- Linguistics
+      WHEN 'LO' THEN 'lao'
+      WHEN 'MG' THEN 'mlg'
+      WHEN 'MO' THEN 'mon'
+      WHEN 'MU' THEN NULL -- MUSIC
+      WHEN 'NO' THEN 'nor'
+      WHEN 'NR' THEN 'nor'
+      WHEN 'NV' THEN 'nav'
+      WHEN 'PG' THEN 'por'
+      WHEN 'PL' THEN 'pol'
+      WHEN 'PN' THEN 'fas'
+      WHEN 'QC' THEN 'quc'
+      WHEN 'RN' THEN 'ron'
       WHEN 'RU' THEN 'rus'
-      WHEN 'SA' THEN 'smo',
-      WHEN 'SC' THEN NULL, -- Scandinavian. Not a specific language.
-      WHEN 'SI' THEN 'swa',
-      WHEN 'SP' THEN 'spa',
-      WHEN 'SR' THEN 'srp',
-      WHEN 'SW' THEN 'swe',
-      WHEN 'TG' THEN 'tgl',
-      WHEN 'TH' THEN 'tha',
-      WHEN 'TJ' THEN 'tgk',
-      WHEN 'TL' THEN 'tel',
-      WHEN 'TN' THEN 'ton',
-      WHEN 'TR' THEN 'tur',
-      WHEN 'TS' THEN 'eng', -- TOEFL. Counting as English.
-      WHEN 'TY' THEN 'tah',
-      WHEN 'UR' THEN 'urd',
-      WHEN 'UZ' THEN 'uzb',
-      WHEN 'VT' THEN 'vie',
+      WHEN 'SA' THEN 'smo'
+      WHEN 'SC' THEN NULL -- Scandinavian. Not a specific language.
+      WHEN 'SI' THEN 'swa'
+      WHEN 'SP' THEN 'spa'
+      WHEN 'SR' THEN 'srp'
+      WHEN 'SW' THEN 'swe'
+      WHEN 'TG' THEN 'tgl'
+      WHEN 'TH' THEN 'tha'
+      WHEN 'TJ' THEN 'tgk'
+      WHEN 'TL' THEN 'tel'
+      WHEN 'TN' THEN 'ton'
+      WHEN 'TR' THEN 'tur'
+      WHEN 'TS' THEN 'eng' -- TOEFL. Counting as English.
+      WHEN 'TY' THEN 'tah'
+      WHEN 'UR' THEN 'urd'
+      WHEN 'UZ' THEN 'uzb'
+      WHEN 'VT' THEN 'vie'
       WHEN 'WL' THEN 'cym'
       ELSE NULL
-    END as language_code
+    END as language_code,
+    titles.callnumber AS inventory_call
   FROM  languages
   JOIN titles ON titles.lang = languages.code;
