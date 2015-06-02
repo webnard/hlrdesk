841530d1416d80ea1f1cb31a42db5afa5c7fad21
Ian Hunter <beanland2@gmail.com>
2015-06-02 11:00:14 -0600

    Postgres passwords allow for special characters.

--- core/app_modules/db.js
+++ core/app_modules/db.js
@@ -2,10 +2,10 @@
 var pg = require('postgres-gen');
 
 var defaults = {};
-defaults.user = process.env.PGUSER;
-defaults.database = process.env.PGDATABASE;
-defaults.password = process.env.PGPASS;
-defaults.host = process.env.PGHOST;
+defaults.user = encodeURIComponent(process.env.PGUSER);
+defaults.database = encodeURIComponent(process.env.PGDATABASE);
+defaults.password = encodeURIComponent(process.env.PGPASS);
+defaults.host = encodeURIComponent(process.env.PGHOST);
 defaults.port = process.env.PGPORT;
 defaults.poolSize = process.env.PGPOOLSIZE || 10;
 defaults.pool = (process.env.PGPOOLSIZE === "0" ? false : 10);
