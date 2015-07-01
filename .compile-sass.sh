96768fcc2695783a778e71fdab1948f8fa6afdde
Ian Hunter <beanland2@gmail.com>
2015-07-01 09:59:27 -0600

    Compressing sass on deployment.

--- .compile-sass.sh
+++ .compile-sass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in core/sass/*.s[ac]ss
+for i in core/sass/main.s[ac]ss
   do
     j=$(basename $i)
-    $(npm bin)/node-sass $i public/css/${j:0:-4}css
+    $(npm bin)/node-sass --output-style compressed $i public/css/${j:0:-4}css
 done
