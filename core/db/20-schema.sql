5df7c364661e256c091024c99fcb4f26d06fec25
Ian Hunter <beanland2@gmail.com>
2016-01-14 12:02:17 -0500

    Fixing problem with non-superuser postgres user running tests.

--- core/db/20-schema.sql
+++ core/db/20-schema.sql
@@ -13,23 +13,6 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
---
--- TOC entry 173 (class 3079 OID 11787)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- TOC entry 1983 (class 0 OID 0)
--- Dependencies: 173
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
