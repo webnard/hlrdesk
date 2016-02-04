f9b7f9f1165aaf2f592525c8635a061bb68802cd
alexcdahl <alex.c.dahl@gmail.com>
2016-02-03 19:29:12 -0700

    Changed due dates to always be at closing time.

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -53,7 +53,6 @@ window.HLRDESK.init.checkout = function initCheckout() {
     for(var i = 0; i<items.length; i++) {
       var dueDate = new Date(items[i].querySelector('input[name=due]').value);
       dueDate.setHours(21);
-      console.log(dueDate);
       toSubmit.push({
         due: dueDate,
         call: items[i].querySelector('input[name=call]').value,
