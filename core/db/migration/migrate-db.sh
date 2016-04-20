69bcec04244f30eefea0673a30452949aaa3b275
Steve Thomas <gamgi1@hotmail.com>
2016-04-20 14:11:32 -0600

    added icn to the migrations call

--- core/db/migration/migrate-db.sh
+++ core/db/migration/migrate-db.sh
@@ -4,7 +4,7 @@ tput setaf 6
 echo "\nAdding Database Migrations"
 tput setaf 5
 
-for i in core/db/migration/*.sql; do 
+for i in core/db/migration/*.sql; do
   echo "Adding file: $i"
   psql -f "$i"
 done
@@ -15,7 +15,7 @@ echo "Implementing Migrations"
 #Note: If this becomes very long, may need to make individual files for readability
 
 psql -c "select addcol('public','users', 'last_login', 'timestamp', 'current_timestamp');"
-psql -c "select addcol('public','media', 'fine_amount', 'real', '0.50');"
+psql -c "select addcol('public','media', 'fine_amount', 'real', '0.50', 'icn');"
 
 tput setaf 6
 echo "Database Migrations Added\033[0m\n"
