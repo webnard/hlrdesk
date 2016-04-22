012aca9b8e35317593df639c3202073477761d34
Steve Thomas <gamgi1@hotmail.com>
2016-04-22 15:58:52 -0600

    removed last param from addcol function in migration call

--- core/db/migration/migrate-db.sh
+++ core/db/migration/migrate-db.sh
@@ -16,7 +16,7 @@ echo "Implementing Migrations"
 
 psql -c "select addcol('public','users', 'last_login', 'timestamp', 'current_timestamp');"
 psql -c "select addcol('public','media', 'fine_amount', 'real', '0.50');"
-psql -c "select addcol('public','inventory', 'icn', 'varchar(16)', '' );"
+psql -c "select addcol('public','inventory', 'icn', 'varchar(16)' );"
 
 tput setaf 6
 echo "Database Migrations Added\033[0m\n"
